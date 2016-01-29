eb56a08a156755a58f24ae4b5ac116b88c2da523
Mauro Alberto Galbiati <jijua77@gmail.com>
2016-01-29 18:01:29 +0100

    Modified UNIFACPropertyPackage.vb.

--- DWSIM/Objects/PropertyPackages/UNIFAC/UNIFACPropertyPackage.vb
+++ DWSIM/Objects/PropertyPackages/UNIFAC/UNIFACPropertyPackage.vb
@@ -35,7 +35,6 @@ Namespace DTL.SimulationObjects.PropertyPackages
         Public m_pr As New Auxiliary.PengRobinson
         Private m_uni As New Auxiliary.UnifacLL
         Private m_lk As New Auxiliary.LeeKesler
-        '<System.NonSerialized()> Private m_xn As DLLXnumbers.Xnumbers
 
         Public Sub New(ByVal comode As Boolean)
             MyBase.New(comode)
@@ -54,7 +53,7 @@ Namespace DTL.SimulationObjects.PropertyPackages
             MyBase.ReconfigureConfigForm()
         End Sub
 
-#Region "    DWSIM Functions"
+#Region "DWSIM Functions"
 
         Public Function RET_KIJ(ByVal id1 As String, ByVal id2 As String) As Double
             If Me.m_pr.InteractionParameters.ContainsKey(id1) Then
@@ -324,7 +323,6 @@ Namespace DTL.SimulationObjects.PropertyPackages
                 Me.CurrentMaterialStream.Phases(phaseID).SPMProperties.enthalpy = result
                 result = Me.m_lk.S_LK_MIX("L", T, P, RET_VMOL(dwpl), RET_VKij, RET_VTC(), RET_VPC(), RET_VW(), RET_VMM(), Me.RET_Sid(298.15, T, P, dwpl))
                 Me.CurrentMaterialStream.Phases(phaseID).SPMProperties.entropy = result
-                'result = Me.m_pr.Z_PR(T, P, RET_VMOL(dwpl), RET_VKij(), RET_VTC, RET_VPC, RET_VW, "L")
                 result = Me.m_lk.Z_LK("L", T / Me.AUX_TCM(dwpl), P / Me.AUX_PCM(dwpl), Me.AUX_WM(dwpl))(0)
                 Me.CurrentMaterialStream.Phases(phaseID).SPMProperties.compressibilityFactor = result
                 resultObj = Me.m_lk.CpCvR_LK("L", T, P, RET_VMOL(dwpl), RET_VKij(), RET_VMAS(dwpl), RET_VTC(), RET_VPC(), RET_VCP(T), RET_VMM(), RET_VW(), RET_VZRa())
@@ -350,7 +348,6 @@ Namespace DTL.SimulationObjects.PropertyPackages
                 Me.CurrentMaterialStream.Phases(phaseID).SPMProperties.enthalpy = result
                 result = Me.m_lk.S_LK_MIX("V", T, P, RET_VMOL(Phase.Vapor), RET_VKij, RET_VTC(), RET_VPC(), RET_VW(), RET_VMM(), Me.RET_Sid(298.15, T, P, Phase.Vapor))
                 Me.CurrentMaterialStream.Phases(phaseID).SPMProperties.entropy = result
-                'result = Me.m_pr.Z_PR(T, P, RET_VMOL(Phase.Vapor), RET_VKij, RET_VTC, RET_VPC, RET_VW, "V")
                 result = Me.m_lk.Z_LK("V", T / Me.AUX_TCM(Phase.Vapor), P / Me.AUX_PCM(Phase.Vapor), Me.AUX_WM(Phase.Vapor))(0)
                 Me.CurrentMaterialStream.Phases(phaseID).SPMProperties.compressibilityFactor = result
                 result = Me.AUX_CPm(Phase.Vapor, T)
@@ -382,9 +379,6 @@ Namespace DTL.SimulationObjects.PropertyPackages
             If phaseID > 0 Then
                 result = overallmolarflow * phasemolarfrac * Me.AUX_MMM(Phase) / 1000 / Me.CurrentMaterialStream.Phases(phaseID).SPMProperties.density.GetValueOrDefault
                 Me.CurrentMaterialStream.Phases(phaseID).SPMProperties.volumetric_flow = result
-            Else
-                'result = Me.CurrentMaterialStream.Phases(phaseID).SPMProperties.massflow.GetValueOrDefault / Me.CurrentMaterialStream.Phases(phaseID).SPMProperties.density.GetValueOrDefault
-                'Me.CurrentMaterialStream.Phases(phaseID).SPMProperties.volumetric_flow = result
             End If
 
         End Sub
@@ -481,7 +475,7 @@ Namespace DTL.SimulationObjects.PropertyPackages
 
 #End Region
 
-#Region "    Auxiliary Functions"
+#Region "Auxiliary Functions"
 
         Function RET_VN(ByVal subst As BaseThermoClasses.Substance) As Object
 
@@ -561,7 +555,7 @@ Namespace DTL.SimulationObjects.PropertyPackages
 
 #End Region
 
-#Region "    Métodos Numéricos"
+#Region "Numerical Methods"
 
         Public Function IntegralSimpsonCp(ByVal a As Double,
                  ByVal b As Double,
@@ -615,7 +609,6 @@ Namespace DTL.SimulationObjects.PropertyPackages
          ByVal Epsilon As Double, ByVal subst As String) As Double
 
             'Cp = A + B*T + C*T^2 + D*T^3 + E*T^4 where Cp in kJ/kg-mol , T in K 
-
 
             Dim Result As Double
             Dim h As Double
